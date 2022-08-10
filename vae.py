19d8a8b954985a55a7294a42df9043ca7fabf0a6
CHENHUI <1366420642@qq.com>
2022-08-10 19:31:35 +0800

    add visualization  and comments , modified code

--- vae.py
+++ vae.py
@@ -21,6 +21,7 @@ class Encoder(torch.nn.Module):
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         mu = self.enc_mu(x)
+
         log_sigma = self.enc_log_sigma(x)
         sigma = torch.exp(log_sigma)
         return torch.distributions.Normal(loc=mu, scale=sigma)
@@ -36,7 +37,9 @@ class Decoder(torch.nn.Module):
     def forward(self, x):
         x = F.relu(self.linear1(x))
         mu = torch.tanh(self.linear2(x))
+        # shape is equals to original input  image
         return torch.distributions.Normal(mu, torch.ones_like(mu))
+        # the  decoder  return  a   distribution
 
 class VAE(torch.nn.Module):
     def __init__(self, encoder, decoder):
@@ -45,9 +48,15 @@ class VAE(torch.nn.Module):
         self.decoder = decoder
 
     def forward(self, state):
-        q_z = self.encoder(state)
-        z = q_z.rsample()
-        return self.decoder(z), q_z
+        self.q_z = self.encoder(state)
+        # print(self.q_z.mean.shape) # ( 128 , 8 )
+
+        z = self.q_z.rsample() # sample a code from latenspace
+        # print(z.shape) # ( 128 , 8 ) ,  最后一个batch由于只有96个,所以返回(96,8)
+
+        return self.decoder(z) , self.q_z
+        # that's need decoder z as close as x ,
+        # meanwhile ,the q_z should be as close as N(0,1)
 
 
 transform = transforms.Compose(
@@ -60,9 +69,9 @@ mnist = torchvision.datasets.MNIST('./', download=True, transform=transform)
 input_dim = 28 * 28
 batch_size = 128
 num_epochs = 100
-learning_rate = 0.001
+learning_rate = 0.01
 hidden_size = 512
-latent_size = 8
+latent_size = 64
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -79,16 +88,24 @@ print('Number of samples: ', len(mnist))
 encoder = Encoder(input_dim, hidden_size, latent_size)
 decoder = Decoder(latent_size, hidden_size, input_dim)
 
-vae = VAE(encoder, decoder).to(device)
+vae = VAE(encoder, decoder).to(device).train()
 
 optimizer = optim.Adam(vae.parameters(), lr=learning_rate)
+fig, axs = plt.subplots(3, 3)
 for epoch in range(num_epochs):
+    vae.train()
+    batch_num = 0
     for data in dataloader:
+        batch_num += 1
         inputs, _ = data
         inputs = inputs.view(-1, input_dim).to(device)
         optimizer.zero_grad()
-        p_x, q_z = vae(inputs)
+        p_x, q_z = vae(inputs) # p(x|z) and q(z)
+        # 这里的decoder 返回的是一个分布,一个基于z 的 x 的分布(即x是变量)
+        # 他的想法是,这个分布,如果把原输入x放进去,出来的probability 应该是最大的
+        # 返回的分布是一个高斯分布,考虑到稳定性,含有e的指数,因此直接使用log来比大小
         log_likelihood = p_x.log_prob(inputs).sum(-1).mean()
+
         kl = torch.distributions.kl_divergence(
             q_z, 
             torch.distributions.Normal(0, 1.)
@@ -97,4 +114,26 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         l = loss.item()
-    print(epoch, l, log_likelihood.item(), kl.item())
+        print(
+            f'[epoch : {epoch+1}]/[{num_epochs}] - batch_num : [{batch_num}] - loss : {l:.3f} - loglike : {log_likelihood.item():.3f} - kl : {kl.item():.3f}'
+        )
+        with torch.no_grad():
+
+            z = vae.q_z.rsample()
+            out_image = vae.decoder(z).mean[:9, :].reshape(9, 28, 28)
+            for i in range(3):
+                for j in range(3):
+                    axs[i, j].imshow(out_image[i*3+j,:,:])
+            fig.tight_layout()  # 调整间距
+            plt.pause(0.05)  # 暂停0.01秒
+            plt.ioff()  # 关闭画图的窗口
+
+            del z, out_image # clear memory
+
+
+
+
+
+
+
+
